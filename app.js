3a9033c877f11a6d9d63a6a28cca1f9aef179e24
Hex_Jack <kkh2003csw@gmail.com>
2024-06-02 07:58:45 +0630

    Ep15 Express redirection and 404 page with app.use

--- app.js
+++ app.js
@@ -13,11 +13,25 @@ app.get('/about', (req, res) => {
         res.sendFile('./views/about.html',{root: __dirname})
     }
 })
+app.get('/about-us', (req, res) => {
+    if (req) {
+        // redirect
+        res.redirect('/about');
+    }
+})
 app.get('/contact', (req, res) => {
     if (req) {
         res.sendFile('./views/contact.html', { root: __dirname })
     }
 })
+// app use for 404 page ***
+app.use((req,res)=>{
+    if(req){
+        res.status(404);
+        res.sendFile('./views/404.html', { root: __dirname })
+    }
+})
+// app.listen for listen server
 app.listen(3000,()=>{
     console.log('app is running on port 3000')
 })
