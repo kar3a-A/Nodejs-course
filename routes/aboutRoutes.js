c774254af1c825c4b02c684d52665ddf0d6a5555
kar3a-A <kkh2003csw@gmail.com>
2024-06-27 14:21:04 +0630

    react basic

--- routes/aboutRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AboutController = require('../controllers/AboutController');
-
-// Route to get the about page
-router.get('/about', AboutController.index);
-
-module.exports = router
--- routes/cartRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const CartController = require('../controllers/CartController');
-
-// Route to get the cart page
-router.get('/cart', CartController.index);
-
-module.exports = router
