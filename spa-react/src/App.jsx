283159fb3caa82161e5a926f1c593d6f180bf028
kar3a-A <kkh2003csw@gmail.com>
2024-06-27 19:24:38 +0630

    react image usage

--- spa-react/src/App.jsx
+++ spa-react/src/App.jsx
@@ -1,10 +1,12 @@
-
+import saitama from './assets/saitama.png'
 import './App.css'
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <img src={saitama} alt="" style={{ width: '300px' }} />
+
     </>
   )
 }
