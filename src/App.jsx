348dab2a304e04d4e22921bb81defa5e406f7b6e
kar3a-A <kkh2003csw@gmail.com>
2024-06-28 08:32:00 +0630

    react use state intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+/* eslint-disable react/no-unknown-property */
 import saitama from './assets/saitama.png'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [name, setName] = useState("Hello world1")
   return (
     <>
-      <h1>Hello World</h1>
+    
+      <h1>{name}</h1>
       <img src={saitama} alt="" style={{ width: '300px' }} />
+      <button onClick={() => setName("Hello world2")}>Change h1 text</button>
 
     </>
   )
