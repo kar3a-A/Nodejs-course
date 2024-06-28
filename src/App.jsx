b3e791bef8afe321c5b8918e6a306b92b645cc44
kar3a-A <kkh2003csw@gmail.com>
2024-06-28 09:08:28 +0630

    working with list react map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
 /* eslint-disable react/no-unknown-property */
-import saitama from './assets/saitama.png'
+// import saitama from './assets/saitama.png'
 import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [name, setName] = useState("Hello world1")
+  const [list, setlist] = useState([
+    {
+      id:1,
+      title: 'Saitama'
+    },
+    {
+      id:2,
+      title: 'Forest'
+    },
+    {
+      id:3,
+      title: 'Book'
+    }
+  ])
+  let changeList = ()=>{
+    setlist([
+      {
+        id:1,
+        title: 'Naruto'
+      },
+      {
+        id:2,
+        title: 'Saseki'
+      },
+      {
+        id:3,
+        title: 'Sakura'
+      }
+    ])
+  }
   return (
     <>
-    
-      <h1>{name}</h1>
-      <img src={saitama} alt="" style={{ width: '300px' }} />
-      <button onClick={() => setName("Hello world2")}>Change h1 text</button>
+      <div>
+        <h3>List</h3>
+        <ul>
+          {list.map((item)=>{
+            return <li key={item.id}>{item.title}</li>
+          })}
+        </ul>
+          <button onClick={()=>changeList() }>Change</button>
+      </div>
 
     </>
   )
