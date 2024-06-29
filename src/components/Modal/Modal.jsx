01795f1010f3f92557cb78ccef2b3ee8b20cba70
kar3a-A <kkh2003csw@gmail.com>
2024-06-29 09:43:33 +0630

    react portal usage changes in index.html as another div rather than root div

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,19 @@
+import { createPortal } from 'react-dom'
 import './Modal.css'
 
 // modal styles with default children content
 // eslint-disable-next-line react/prop-types
 const Modal = ({children,setshowModal}) => {
   return (
-    <div className="modal-backdrop">
-      <div className="modal">
-        {children}
-        <button onClick={() => setshowModal(false)}>Close</button>
+    createPortal(
+      <div className="modal-backdrop">
+        <div className="modal">
+          {children}
+          <button onClick={() => setshowModal(false)}>Close</button>
+        </div>
       </div>
-    </div>
+    , document.getElementById('modal')
+  )
   );
 }
 
