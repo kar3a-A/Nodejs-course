b33a12e487993352ccc73793a4b2442b9533242d
kar3a-A <kkh2003csw@gmail.com>
2024-06-30 15:36:27 +0630

    two way data binding with value= {} and onchange={}

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -9,11 +9,15 @@ const PostForm = () => {
         <h2>Create Post</h2>
         <div className={styles.formControl}>
             <label htmlFor="">Title</label>
-            <input type="text" onChange={(e)=> settitle(e.target.value)}/>
+            <input type="text" onChange={(e)=> settitle(e.target.value)} value={title}/>
         </div>
         <p>{title}</p>
+
         <div className={styles.formControl}>
-            <button >Post Now</button>
+            <button style={{marginRight:3}}>
+              Post Now
+            </button>
+            <button type='button' onClick={()=>settitle(" ")}>Clear</button>
         </div>
       </form>
     </>
