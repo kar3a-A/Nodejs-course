f1e54665bf054b5dc0b526ce0b72bf44e45e7b12
kar3a-A <kkh2003csw@gmail.com>
2024-07-01 18:18:37 +0630

    react useeffect detail of dependency array useEffect(()=>{},[url])
    [url] detail

--- src/components/TripList/TripList.jsx
+++ src/components/TripList/TripList.jsx
@@ -2,28 +2,33 @@ import { useState, useEffect } from 'react'
 import styles from './TripList.module.css'
 
 
-const TripList = () => {
-    const [trips, setTrips] = useState([])
 
+const TripList = () => {
+
+    const [trips, setTrips] = useState([])
+    const [url, setUrl] = useState('http://localhost:3000/trips')
     useEffect(() => {
-        fetch('http://localhost:3000/trips')
+        fetch(url)
         .then(response => response.json())
         .then(data => {
             setTrips(data)
         })
         .catch(err => console.log(err))
-    }, [])
-
+    }, [url])
   return (
     <div>
         <h1>Ready to go?</h1>
         <h3>Trip List</h3>
+        <button onClick={() => setUrl('http://localhost:3000/trips')}>Get Trips</button>
+        <button onClick={() => setUrl(`http://localhost:3000/trips?location=beach`)}>
+            Beach
+        </button>
         <ul>
             {
                 trips.map(trip => (
                     <li key={trip.id} className={styles.trip}>
                         <h3>{trip.name}</h3>
-                        <p>Price: ${trip.price}</p>
+                        <p>Price: ${trip.price} | location: {trip.location}</p>
                     </li>
                 ))
             }
