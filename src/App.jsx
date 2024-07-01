ead137a9cca58c119a889d2f9bb9dda2919068ed
kar3a-A <kkh2003csw@gmail.com>
2024-07-01 14:42:37 +0630

    json server setup and new project create

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,12 @@
-import { useState } from "react"
-import Navbar from "./components/Navbar/Navbar"
-import PostsList from "./components/PostsList/PostsList"
-import Modal from "./components/Modal/Modal"
-import PostForm from "./components/PostForm/PostForm"
 
+import './App.css'
 
+function App() {
 
-
-
-const App = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [posts, setposts] = useState([
-    {
-      id: 1,
-      title: "Manga",
-      status: "ongoing"
-    },
-    {
-      id: 2,
-      title: "Book",
-      status: "dropped"
-    },
-
-  ])
-
-  let addPost = (post) =>{
-    setposts((prev)=>[...prev, post ])
-    setshowModal(false)
-  }
-  const [showModal, setshowModal] = useState(false)
-  // eslint-disable-next-line no-unused-vars
-  const [danger, setdanger] = useState(true)
 
   return (
     <>
-      <Navbar setshowModal={setshowModal}/>
-      <PostsList posts={posts}/>
-      {
-        showModal && 
-        <Modal danger={danger} setshowModal={setshowModal}>
-          { /* Modal content */ }
-          <PostForm addPost={addPost}/>
-        </Modal>
-      }
-
+      <h2>Hello</h2>
     </>
   )
 }
