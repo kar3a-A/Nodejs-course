a91b03bd741649b5b292c9a274ccd87e3f3720cf
kar3a-A <kkh2003csw@gmail.com>
2024-07-08 17:31:50 +0630

    mongodb setup and connecting with mongoose

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,21 @@ const morgan = require("morgan");
 require("dotenv").config();
 const { route } = require("./routes/receipes");
 const receipeRoutes = require("./routes/receipes");
+const mongoose = require("mongoose");
 
 const app = express();
+const mongoURL = "mongodb+srv://kkh2003csw:test1234@mern-cluster.ssr4k0f.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Cluster"
+
+mongoose.connect(mongoURL).then(()=>{
+    console.log("Connected to mongoDB");
+    // app.listen(4000);
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
 app.use(morgan("dev"));
 
 app.use('/api/receipes',receipeRoutes);
 
-// app.listen(4000);
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
+
+
