c211446c862b2b3f68faf0167cae40ec14f57f2f
kar3a-A <kkh2003csw@gmail.com>
2024-07-12 20:49:17 +0630

    add ingredient one by one feature

--- frontend/src/pages/ReceipeCreate.jsx
+++ frontend/src/pages/ReceipeCreate.jsx
@@ -1,7 +1,16 @@
+import { useRef, useState } from 'react'
 import plus from '../assets/plus.svg'
 import Ingredient from '../components/Ingredient'
 
-const ReceipeCreate = ({ingredients}) => {
+const ReceipeCreate = () => {
+  const [ingredients, setIngredients] = useState([]);
+  const [newIngredient, setNewIngredient] = useState('');
+  const addIngredient = () =>{
+    setIngredients((prev)=>{
+      return [...prev, newIngredient]
+    });
+    setNewIngredient('');
+  }
   return (
     <div className="rounded-md mx-auto max-w-md border-2 border-white p-4">
       <h1 className="mb-6 text-2xl font-bold text-lime-500 text-center">Recipe Create Form</h1>
@@ -9,15 +18,21 @@ const ReceipeCreate = ({ingredients}) => {
           <input type="text" placeholder="Recipe Title" className="w-full p-1" />
           <textarea placeholder="Recipe Description" rows="5" className="w-full p-1" />
           <div className="flex space-x-2 items-center">
-              <input type="text" placeholder="Recipe Ingredient" className="w-full p-1" />
+              <input 
+                value={newIngredient} 
+                type="text" 
+                placeholder="Recipe Ingredient"
+                onChange={e=>setNewIngredient(e.target.value)}
+                className="w-full p-1" />
               <img src={plus} 
                 title="Add Ingredient"
                 width="40" 
                 alt="" 
+                onClick={newIngredient.length >3 ? addIngredient : null}
                 className='cursor-pointer rounded-md hover:bg-green-200'/>
           </div>
           <div>
-              <Ingredient ingredients={['flour','salt']}/>
+              <Ingredient ingredients={ingredients}/>
           </div>
           <button className='w-full px-3 py-1 rounded-full bg-green-300 text-white hover:bg-lime-300'>
             Create Recipe
