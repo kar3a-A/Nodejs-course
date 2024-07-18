22f331b6408deec9e12a4fea298d7c8ebd600b4f
kar3a-A <kkh2003csw@gmail.com>
2024-07-18 17:53:25 +0630

    register user api with encrypt password using bcryptjs

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,12 +1,33 @@
-// import User from "../models/User";
+const User =  require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const UserController = {
     login: (req, res)=>{
         return res.json({message: "login"})
     },
 
-    register: (req, res)=>{
-        return res.json({ message: "register" })
+    register: async(req, res)=>{
+        try{
+            let { name, email, password } = req.body
+            let userExists = await User.findOne({ email });
+            if (userExists) {
+                throw "User already exists"
+            }
+            salt = await bcrypt.genSalt()
+            hashPw = await bcrypt.hash(password, salt)
+            let newUser = await User.create({
+                name,
+                email,
+                password: hashPw
+            })
+
+            return res.json(newUser)
+        } catch(err) {
+            return res.status(400).json({
+                error: err
+            })
+        }
+
     }
 
 }
